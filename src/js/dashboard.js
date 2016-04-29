fd0b1833819ff7078ed0239e7ea736e1ebb13765
askeing <askeing@gmail.com>
2016-04-29 15:55:55 +0800

    Abstract the generatePage() func

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -3,31 +3,18 @@
 var config = new Object();
 var repos = [];
 
-$(document).ready(function() {
-    $.getJSON( "config/config.json", function( data ) {
-        $.each( data, function( key, val ) {
-            config[key] = val;
-        });
+function generatePage(input_json) {
+    var all_languages = new Map();
+    var container = $("#repos_list");
 
-        // Change the URL link
-        $("#btn-learn-more").click( function() {
-            window.open(config["url"], "githubURL");
-        });
+    repos = input_json;
+    var row_div = document.createElement("div");
+    row_div.className = "row";
+    container.html("");
+    container.append(row_div);
+    var container_row = container.children("div.row");
 
-        // get repos information
-        var all_languages = new Map();
-        var api_url = "https://api.github.com/users/" + config["username"] + "/repos?type=all&per_page=100&sort=updated&direction=desc";
-        var container = $("#repos_list");
-        $.ajax(api_url)
-            .done( function(ret_json) {
-                repos = ret_json;
-                var row_div = document.createElement("div");
-                row_div.className = "row";
-                container.html("");
-                container.append(row_div);
-                var container_row = container.children("div.row");
-
-                var repo_template = ' \
+    var repo_template = ' \
                 <div class="col-md-4 col-xs-6 repo-card lang-%REPO_LANG_ID%"> \
                     <div class="panel panel-default"> \
                         <div class="panel-heading" repolink="%REPO_LINK%"> \
@@ -44,65 +31,83 @@ $(document).ready(function() {
                     </div> \
                 </div>';
 
-                $.each( repos, function (index, repo) {
-                    var name = repo.name;
-                    var html_url = repo.html_url;
-                    var description = repo.description || "No description provided.";
-                    var stargazers = repo.stargazers_count || "0";
-                    var forks = repo.forks || "0";
-                    var language = repo.language || "Other";
-                    var updated_at = repo.updated_at;
-                    // Add language into all_languates list.
-                    var language_id = language.replace(/[\s]+/gi, "")
-                        .replace(/[\+]/gi, "plus")
-                        .replace(/[#]/gi, "sharp")
-                        .replace(/[']/gi, "apostrophe");
-                    all_languages.set(language, language_id);
-                    // Create repo card html.
-                    var replace = {
-                        "%REPO_NAME%": name,
-                        "%REPO_DESC%": description,
-                        "%REPO_LANG%": language,
-                        "%REPO_LANG_ID%": language_id,
-                        "%REPO_FORK%": forks,
-                        "%REPO_STAR%": stargazers,
-                        "%REPO_LINK%": html_url,
-                        "%REPO_UPTATE%": updated_at
-                    };
-                    var repo_card_html = repo_template.replace(/%\w+%/g, function(placeholder) { return replace[placeholder] || placeholder; } );
-                    container_row.append(repo_card_html);
-                });
+    $.each( repos, function (index, repo) {
+        var name = repo.name;
+        var html_url = repo.html_url;
+        var description = repo.description || "No description provided.";
+        var stargazers = repo.stargazers_count || "0";
+        var forks = repo.forks || "0";
+        var language = repo.language || "Other";
+        var updated_at = repo.updated_at;
+        // Add language into all_languates list.
+        var language_id = language.replace(/[\s]+/gi, "")
+            .replace(/[\+]/gi, "plus")
+            .replace(/[#]/gi, "sharp")
+            .replace(/[']/gi, "apostrophe");
+        all_languages.set(language, language_id);
+        // Create repo card html.
+        var replace = {
+            "%REPO_NAME%": name,
+            "%REPO_DESC%": description,
+            "%REPO_LANG%": language,
+            "%REPO_LANG_ID%": language_id,
+            "%REPO_FORK%": forks,
+            "%REPO_STAR%": stargazers,
+            "%REPO_LINK%": html_url,
+            "%REPO_UPTATE%": updated_at
+        };
+        var repo_card_html = repo_template.replace(/%\w+%/g, function(placeholder) { return replace[placeholder] || placeholder; } );
+        container_row.append(repo_card_html);
+    });
 
-                function addLanguageButtons(lang_id, lang_key, all_languages) {
-                    if (lang_key != null) {
-                        var id = "lang-" + lang_id;
-                        var li_html = "<li id=\"" + id + "\"><a href=\"#\">" + lang_key + "</a></li>";
-                        $("ul#btn-language-selection").append(li_html);
-                    }
-                }
-                all_languages.forEach(addLanguageButtons);
+    function addLanguageButtons(lang_id, lang_key, all_languages) {
+        if (lang_key != null) {
+            var id = "lang-" + lang_id;
+            var li_html = "<li id=\"" + id + "\"><a href=\"#\">" + lang_key + "</a></li>";
+            $("ul#btn-language-selection").append(li_html);
+        }
+    }
+    all_languages.forEach(addLanguageButtons);
 
-                // Listen on repo card click
-                $("#repos_list div.row div[repolink]").click(function() {
-                    var repo_link = $(this).attr("repolink");
-                    window.open(repo_link, "githubURL");
-                });
+    // Listen on repo card click
+    $("#repos_list div.row div[repolink]").click(function() {
+        var repo_link = $(this).attr("repolink");
+        window.open(repo_link, "githubURL");
+    });
 
-                // listen on language selection buttons
-                $("ul#btn-language-selection li").click(function () {
-                    // TODO: implement the filter of repos cards
-                    var select_lang = this.id;
-                    var select_lang_text = $(this).text();
-                    if (select_lang === "lang-all") {
-                        $("#repos_list .repo-card").show();
-                        $("#btn-language-label").text(select_lang_text);
-                    }
-                    else {
-                        $("#repos_list .repo-card").hide();
-                        $("#repos_list .repo-card." + select_lang).show();
-                        $("#btn-language-label").text(select_lang_text);
-                    }
-                });
+    // listen on language selection buttons
+    $("ul#btn-language-selection li").click(function () {
+        // TODO: implement the filter of repos cards
+        var select_lang = this.id;
+        var select_lang_text = $(this).text();
+        if (select_lang === "lang-all") {
+            $("#repos_list .repo-card").show();
+            $("#btn-language-label").text(select_lang_text);
+        }
+        else {
+            $("#repos_list .repo-card").hide();
+            $("#repos_list .repo-card." + select_lang).show();
+            $("#btn-language-label").text(select_lang_text);
+        }
+    });
+}
+
+$(document).ready(function() {
+    $.getJSON( "config/config.json", function( data ) {
+        $.each( data, function( key, val ) {
+            config[key] = val;
+        });
+
+        // Change the URL link
+        $("#btn-learn-more").click( function() {
+            window.open(config["url"], "githubURL");
+        });
+
+        // get repos information
+        var api_url = "https://api.github.com/users/" + config["username"] + "/repos?type=all&per_page=100&sort=updated&direction=desc";
+        $.ajax(api_url)
+            .done( function(ret_json) {
+                generatePage(ret_json);
             })
             .fail( function(ret_json) {
                 // TODO: un-testing
@@ -111,7 +116,7 @@ $(document).ready(function() {
                 error_div.className = "alert alert-danger";
                 error_div.setAttribute("role", "alert");
                 error_div.textContent = error_msg;
-                container.html(error_div);
+                $("#repos_list").html(error_div);
             });
         $('.dropdown-toggle').dropdown();
     });
